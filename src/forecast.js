1be540601557d377e2a4870fdf13362b0f025a00
Aiden Mayoros <aiden.mayoros@gmail.com>
2022-01-11 17:20:38 -0700

    css changes

--- src/forecast.js
+++ src/forecast.js
@@ -1,14 +1,15 @@
 function Forecast({ day }) {
+  console.log(day);
   const img = `http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`;
 
   const daysDate = new Date(day.dt * 1000).toString().split(" ");
 
   return (
-    <li key={day.dt} className="forecastDay">
+    <li key={day.dt} className='forecastDay'>
       <h1>{daysDate[0]}</h1>
       <h2>{Math.round(day.temp.day)}°F</h2>
       <p>{day.weather[0].description}</p>
-      <img src={img} alt="weather"></img>
+      <img src={img} alt='weather'></img>
     </li>
   );
 }
