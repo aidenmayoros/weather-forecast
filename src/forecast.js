71b4b06390e465e1eb69d39a09088ceb4a4ac700
Aiden Mayoros <aiden.mayoros@gmail.com>
2022-01-26 15:24:30 -0700

    cleaned up CSS and added date updates

--- src/forecast.js
+++ src/forecast.js
@@ -1,17 +1,25 @@
 function Forecast({ day }) {
-  console.log(day);
-  const img = `http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`;
+	const img = `http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`;
 
-  const daysDate = new Date(day.dt * 1000).toString().split(" ");
+	const daysDate = new Date(day.dt * 1000).toString().split(" ");
+	const dateList = [daysDate[0], daysDate[1], daysDate[2]];
+	const date = dateList.join(" ");
 
-  return (
-    <li key={day.dt} className='forecastDay'>
-      <h1>{daysDate[0]}</h1>
-      <h2>{Math.round(day.temp.day)}°F</h2>
-      <p>{day.weather[0].description}</p>
-      <img src={img} alt='weather'></img>
-    </li>
-  );
+	let weatherDescription = day.weather[0].description
+		.split(" ")
+		.map((word) => {
+			return word[0].toUpperCase() + word.substring(1);
+		})
+		.join(" ");
+
+	return (
+		<li className='forecastDay'>
+			<h1 className='date'>{date}</h1>
+			<h2> {Math.round(day.temp.day)}°F</h2>
+			<p className='weatherDescription'>{weatherDescription}</p>
+			<img className='weatherImage' src={img} alt='weather'></img>
+		</li>
+	);
 }
 
 export default Forecast;
--- src/header.js
+++ src/header.js
@@ -1,9 +1,9 @@
 function Header() {
-  return (
-    <div id="ForecastContainer">
-      <h1 id="forecast-header">5-Day Forecast for Phoenix Arizona</h1>
-    </div>
-  );
+	return (
+		<div id='ForecastContainer'>
+			<h1 id='forecast-header'>7-Day Forecast for Phoenix Arizona</h1>
+		</div>
+	);
 }
 
 export default Header;
